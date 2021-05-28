04e797fb244a48f29700127d25fffe27e985bb80
Felipe S. Spina <felipess4@al.insper.edu.br>
2021-05-27 21:10:04 -0300

    Implementação dos tiros da nave amiga

--- SpaceRun.py
+++ SpaceRun.py
@@ -17,7 +17,11 @@ NAVE_WIDTH = 130 #Largura da nave
 
 NAVE_HEIGHT = 140 #Altura do nave
 
-planeta1_fundo = pygame.image.load('imgs/Fundo1.png').convert_alpha()
+TIRO_WIDTH = 50 #Largura dos tiros
+
+TIRO_HEIGHT = 50 #Altura dos tiros
+
+planeta1_fundo = pygame.image.load('imgs/Fundo1.jpg').convert_alpha()
 planeta1_fundo = pygame.transform.scale(planeta1_fundo, (WIDTH, HEIGHT))
 planeta2_fundo = pygame.image.load('imgs/Fundo2.png').convert_alpha()
 planeta2_fundo = pygame.transform.scale(planeta2_fundo, (WIDTH, HEIGHT))
@@ -33,10 +37,14 @@ naveaamiga = pygame.image.load ('imgs/Nave_amiga.png').convert_alpha()
 naveaamiga = pygame.transform.scale (naveaamiga, (NAVE_WIDTH, NAVE_HEIGHT))
 naveinimiga = pygame.image.load ('imgs/Nave_inimiga.png').convert_alpha()
 naveinimiga = pygame.transform.scale (naveinimiga, (NAVE_WIDTH, NAVE_HEIGHT))
+tiro_amigo = pygame.image.load ('imgs/Tiro amigo.png').convert_alpha()#Tiro do player 
+tiro_amigo = pygame.transform.scale (tiro_amigo, (TIRO_WIDTH, TIRO_HEIGHT)) #Ajuste do tamanho da imagem do tiro do player
+tiro_inimigo = pygame.image.load ('imgs/Tiro inimigo.png').convert_alpha() #Tiro do player 
+tiro_inimigo = pygame.transform.scale (tiro_inimigo, (TIRO_WIDTH, TIRO_HEIGHT)) #Ajuste do tamanho da imagem do tiro do player
 
 
 class amigo(pygame.sprite.Sprite):
-    def __init__(self,img):
+    def __init__(self,img,all_sprites,all_tiros,img_tiro):
         pygame.sprite.Sprite.__init__(self)
         self.image = img
         self.rect = self.image.get_rect()
@@ -44,6 +52,9 @@ class amigo(pygame.sprite.Sprite):
         self.rect.left = 0
         self.speedx = 0
         self.speedy = 0
+        self.all_sprites = all_sprites
+        self.all_tiros = all_tiros
+        self.tiro_img = img_tiro
     def update(self):
         self.rect.x += self.speedx
         self.rect.y += self.speedy
@@ -55,6 +66,10 @@ class amigo(pygame.sprite.Sprite):
             self.rect.right = WIDTH//2
         if self.rect.left < 0:
             self.rect.left = 0
+    def tiro(self):
+        bala = Tiro_Amigo(self.tiro_img, self.rect.bottom, self.rect.centerx)
+        self.all_sprites.add(bala)
+        self.all_tiros.add(bala)
 
 class inimigo(pygame.sprite.Sprite):
     def __init__(self,img):
@@ -66,44 +81,76 @@ class inimigo(pygame.sprite.Sprite):
         self.speedx = -3
     def update(self):
         self.rect.x += self.speedx
-        if self.rect.left < 1100:
-            self.rect.x = 1100
+        if self.rect.left < 1000:
+            self.rect.x = 1000
         self.speedx = -3
 
 
+class Tiro_Amigo(pygame.sprite.Sprite):
+    def __init__(self, img, bottom, centerx):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = img
+        self.rect = self.image.get_rect()
+        self.rect.centerx = centerx
+        self.rect.bottom = bottom
+        self.speedx = 8  
+    def update(self):
+        self.rect.x += self.speedx
+        if self.rect.right < 0:
+            self.kill()
 
 all_sprites = pygame.sprite.Group()
+all_tiros = pygame.sprite.Group()
+all_inimigos = pygame.sprite.Group()
 
 
 
-player_nave = amigo(naveaamiga)
+player_nave = amigo(naveaamiga, all_sprites, all_tiros, tiro_amigo)
 all_sprites.add(player_nave)
 
 for i in range(2):
     inimigo_nave = inimigo(naveinimiga)
     all_sprites.add(inimigo_nave)
+    all_inimigos.add(inimigo_nave)
 
-
-FPS = 30
-velocidade = 2
+velocidade = 3.5 #Velocidade 
+velocidade2 = 2  #Velocidade2 
 game = True
+FPS = 30
 clock = pygame.time.Clock()
 while game:
     clock.tick(FPS)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             game = False
-    if event.type == pygame.KEYDOWN:
-        if event.key == pygame.K_DOWN:
-            player_nave.speedy += velocidade
-        if event.key == pygame.K_UP:
-            player_nave.speedy -= velocidade
-        if event.key == pygame.K_RIGHT:
-            player_nave.speedx += velocidade
-        if event.key == pygame.K_LEFT:
-            player_nave.speedx -= velocidade
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_DOWN:
+                player_nave.speedy += velocidade2
+            if event.key == pygame.K_UP:
+                player_nave.speedy -= velocidade2
+            if event.key == pygame.K_RIGHT:
+                player_nave.speedx += velocidade
+            if event.key == pygame.K_LEFT:
+                player_nave.speedx -= velocidade
+            if event.key == pygame.K_SPACE:
+                player_nave.tiro()
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_DOWN:
+                player_nave.speedy += velocidade2
+            if event.key == pygame.K_UP:
+                player_nave.speedy -= velocidade2
+            if event.key == pygame.K_RIGHT:
+                player_nave.speedx += velocidade
+            if event.key == pygame.K_LEFT:
+                player_nave.speedx -= velocidade
 
     all_sprites.update()
+    hits = pygame.sprite.groupcollide(all_inimigos, all_tiros, True, True)
+    
+    for Enemy in hits:
+        i = inimigo(naveinimiga)
+        all_sprites.add(i)
+        all_inimigos.add(i)
 
     tela.blit(planeta1_fundo, (0, 0))
     all_sprites.draw(tela)
