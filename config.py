1555e8492257c540ce4f161fe3beb60dcb76f5d8
Gabriela <gabrielakk@al.insper.edu.br>
2021-06-09 17:59:41 -0300

    configurações gerais do jogo

---
+++ config.py
@@ -0,0 +1,28 @@
+#dados gerais do jogo 
+WIDTH = 1252
+
+FPS = 30
+
+HEIGHT = 556
+
+#tamanho dos componentes do jogo
+NAVE_WIDTH = 80 #Largura da nave
+
+NAVE_HEIGHT = 90 #Altura do nave
+
+TIRO_WIDTH = 30 #Largura dos tiros
+
+TIRO_HEIGHT = 30 #Altura dos tiros
+
+#estados para controle do fluxo da aplicação 
+INIT = 0 
+GAME = 1 
+QUIT = 2 
+END_SCREEN = 3
+END_SCREEN2 = 4
+INTRODUCTION = 5
+TRANSICAO1 = 6
+TRANSICAO2 = 7
+TRANSICAO3 = 8
+GAME2 = 9
+GAME3 = 10
